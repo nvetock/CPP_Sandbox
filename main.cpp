2ec2593b29bd189084a173675307875c91d4e8bc
straws <NVetock1@gmail.com>
2025-04-09 22:43:22 -0500

    Small exploration of encapsulation principles and early class creation.

--- main.cpp
+++ main.cpp
@@ -5,8 +5,28 @@
 #include "Calendar.h"
 
 #include <iostream>
+#include <string>
 
+const std::string& firstAlphabetical(const std::string& a, const std::string& b)
+{
+	return (a < b) ? a : b;
+}
 
+class Employee
+{
+private:
+	std::string m_name{};
+	char m_firstInitial{};
+
+public:
+
+	void setName(std::string_view name) { 
+		m_name = name;
+		m_firstInitial = name[0];
+	}
+	const std::string& getName() const { return m_name; }
+	const char& getInitial() const { return m_firstInitial; }
+};
 
 int main()
 {
@@ -15,6 +35,7 @@ int main()
 	// EnumTests::test_EnumTests();
 	// StructTests::test_StructTests();
 
+	/*
 	Calendar cal = Calendar();
 
 	cal.setDay(31);
@@ -22,6 +43,20 @@ int main()
 	cal.setYear(2025);
 
 	std::cout << cal.print() << std::endl;
+	*/
+
+	std::string hello{ "Hello" };
+	std::string world{ "World" };
+
+	std::cout << firstAlphabetical(hello, world);
+
+	Employee e = Employee();
+
+	std::cout << e.getName() << '\n';
+	std::cout << e.getInitial() << '\n';
+	e.setName("Mark");
+	std::cout << e.getName() << '\n';
+	std::cout << e.getInitial() << '\n';
 
 	return 0;
 }
